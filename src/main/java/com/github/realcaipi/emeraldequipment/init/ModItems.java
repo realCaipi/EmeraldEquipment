218c4066800e32f65c815261e677ff71eeebd0d2
Rokcas <rokas0090@gmail.com>
2020-01-21 02:18:33 +0200

    Add functional emerald equipment

--- src/main/java/com/github/realcaipi/emeraldequipment/init/ModItems.java
+++ src/main/java/com/github/realcaipi/emeraldequipment/init/ModItems.java
@@ -2,8 +2,15 @@ package com.github.realcaipi.emeraldequipment.init;
 
 import com.github.realcaipi.emeraldequipment.EmeraldEquipment;
 import com.github.realcaipi.emeraldequipment.item.ModItemTier;
+import com.github.realcaipi.emeraldequipment.item.armor.ModArmorMaterial;
 
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
 import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,12 +21,20 @@ public class ModItems {
 
     public static final RegistryObject<SwordItem> EMERALD_SWORD = ITEMS.register("emerald_sword", () ->
         new SwordItem(ModItemTier.EMERALD, 3, -2.2f, new SwordItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
-
-
-
-    /*new SwordItem(ItemTier.DIAMOND, 10, -2.5f, 
-				new SwordItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP))
-			setup( new ArmorItem(ArmorMaterialList.EMERALD, EquipmentSlotType.CHEST,
-				new ArmorItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "emerald_chestplate"
-			)*/
+    public static final RegistryObject<PickaxeItem> EMERALD_PICKAXE = ITEMS.register("emerald_pickaxe", () ->
+        new PickaxeItem(ModItemTier.EMERALD, 1, -2.6f, new PickaxeItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<AxeItem> EMERALD_AXE = ITEMS.register("emerald_axe", () ->
+        new AxeItem(ModItemTier.EMERALD, 5, -2.8f, new AxeItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<ShovelItem> EMERALD_SHOVEL = ITEMS.register("emerald_shovel", () ->
+        new ShovelItem(ModItemTier.EMERALD, 1.5f, -2.8f, new ShovelItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<HoeItem> EMERALD_HOE = ITEMS.register("emerald_hoe", () ->
+        new HoeItem(ModItemTier.EMERALD, 0, new HoeItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<ArmorItem> EMERALD_HELMET = ITEMS.register("emerald_helmet", () ->
+        new ArmorItem(ModArmorMaterial.EMERALD, EquipmentSlotType.HEAD, new ArmorItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<ArmorItem> EMERALD_CHESTPLATE = ITEMS.register("emerald_chestplate", () ->
+        new ArmorItem(ModArmorMaterial.EMERALD, EquipmentSlotType.CHEST, new ArmorItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<ArmorItem> EMERALD_LEGGINGS = ITEMS.register("emerald_leggings", () ->
+        new ArmorItem(ModArmorMaterial.EMERALD, EquipmentSlotType.LEGS, new ArmorItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+    public static final RegistryObject<ArmorItem> EMERALD_BOOTS = ITEMS.register("emerald_boots", () ->
+        new ArmorItem(ModArmorMaterial.EMERALD, EquipmentSlotType.FEET, new ArmorItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));   
 }
--- src/main/java/com/github/realcaipi/emeraldequipment/item/armor/ModArmorMaterial.java
+++ src/main/java/com/github/realcaipi/emeraldequipment/item/armor/ModArmorMaterial.java
@@ -14,10 +14,29 @@ import net.minecraft.item.Items;
 //99% of this code copy pasted from vanilla ArmorMaterial enum
 //There must be a better way to do this
 public enum ModArmorMaterial implements IArmorMaterial {
-    EMERALD("testmod:emerald", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, () -> {
-       return Ingredient.fromItems(Items.LEATHER);
+    EMERALD("emeraldequipment:emerald", 15, new int[]{3, 6, 8, 3}, 20, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.5F, () -> {
+       return Ingredient.fromItems(Items.EMERALD);
     });
- 
+/*
+    LEATHER("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, () -> {
+      return Ingredient.fromItems(Items.LEATHER);
+   }),
+   CHAIN("chainmail", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, () -> {
+      return Ingredient.fromItems(Items.IRON_INGOT);
+   }),
+   IRON("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, () -> {
+      return Ingredient.fromItems(Items.IRON_INGOT);
+   }),
+   GOLD("gold", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, () -> {
+      return Ingredient.fromItems(Items.GOLD_INGOT);
+   }),
+   DIAMOND("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, () -> {
+      return Ingredient.fromItems(Items.DIAMOND);
+   }),
+   TURTLE("turtle", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, () -> {
+      return Ingredient.fromItems(Items.SCUTE);
+   });
+ */
     private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
     private final String name;
     private final int maxDamageFactor;
